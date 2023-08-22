7b4673a20c5123859c4f6150a2fe08864442b9e4
getenjr <32555548+getenjr@users.noreply.github.com>
2023-08-23 03:58:53 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -139,8 +139,8 @@ custom_packages() {
     # Add p7zip
 svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 
-# Add luci-app-tinyfilemanager
-# svn co https://github.com/lynxnexy/luci-app-tinyfilemanager/trunk package/luci-app-tinyfilemanager
+#Add luci-app-tinyfilemanager
+svn co https://github.com/lynxnexy/luci-app-tinyfilemanager/trunk package/luci-app-tinyfilemanager
 svn co https://github.com/helmiau/helmiwrt-packages/trunk/luci-app-tinyfm package/luci-app-tinyfm
 svn co https://github.com/helmiau/helmiwrt-packages/trunk/luci-app-libernet-plus package/luci-app-libernet-plus
 svn co https://github.com/helmiau/helmiwrt-packages/trunk/luci-app-libernet-bin package/luci-app-libernet-bin
